ca2073353559207d51ead22c92ee0e1e6b0bd6f2
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2021-08-29 21:12:04 +0200

    master props configuration and depend on default value

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,10 +1,8 @@
 <template>
   <li>
-    <h2>{{ name }} {{ friendIsFavorite === '1' ? '(Favorite)': ''}}</h2>
-    <button @click="toggleFavorite">
-      Toggle Favorite
-    </button>
-     <button @click="toggleDetails">
+    <h2>{{ name }} {{ friendIsFavorite === "1" ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetails">
       {{ detailsAreVisible ? "Hide" : "Show" }} Details
     </button>
     <ul v-if="detailsAreVisible">
@@ -16,7 +14,30 @@
 
 <script>
 export default {
-  props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
+  // https://v3.vuejs.org/guide/component-props.html#prop-types
+  // props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    }, // can be String only instead of object
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: String,
+      required: false,
+      default: "0", // can be function
+      validator: function (value) {
+        return value === "1" || value === "0";
+      },
+    },
+  },
   data() {
     return {
       detailsAreVisible: false,
@@ -26,21 +47,21 @@ export default {
         phone: "0123 45678 90",
         email: "manuel@localhost.com",
       },
-      friendIsFavorite: this.isFavorite // just initial value
+      friendIsFavorite: this.isFavorite, // just initial value
     };
   },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
-    toggleFavorite(){
-      // this will not update data in parent 
-      if(this.friendIsFavorite === '1'){
-        this.friendIsFavorite= '0';
-      }else{
-        this.friendIsFavorite='1';
+    toggleFavorite() {
+      // this will not update data in parent
+      if (this.friendIsFavorite === "1") {
+        this.friendIsFavorite = "0";
+      } else {
+        this.friendIsFavorite = "1";
       }
-    }
+    },
   },
 };
 </script>
