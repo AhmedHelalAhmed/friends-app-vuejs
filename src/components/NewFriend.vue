c845b017e093167920d44f1a548ddb8d0e2f0f99
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2021-08-31 00:04:10 +0200

    Add new contact implementation

---
+++ src/components/NewFriend.vue
@@ -0,0 +1,55 @@
+<template>
+  <section>
+    <form @submit.prevent="submitData">
+      <div>
+        <label> Name: </label>
+
+        <input v-model="name" type="text" />
+      </div>
+      <div>
+        <label> Phone: </label>
+
+        <input v-model="phone" type="tel" />
+      </div>
+      <div>
+        <label> E-mail: </label>
+
+        <input v-model="email" type="email" />
+      </div>
+
+      <div>
+        <button type="submit">Add contact</button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  emits: ["add-contact"],
+  data() {
+    return {
+      name: "",
+      phone: "",
+      email: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit("add-contact", {
+        id: new Date().toISOString(),
+        name: this.name,
+        phone: this.phone,
+        email: this.email,
+        isFavorite: false,
+      });
+      this.resetForm();
+    },
+    resetForm() {
+      this.name = "";
+      this.phone = "";
+      this.email = "";
+    },
+  },
+};
+</script>
